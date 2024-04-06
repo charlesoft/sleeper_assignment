bd50f55c3347a69ee484b4d3bf8814d2caaf2500
Charles Furtado <charlees1509@hotmail.com>
2024-04-06 18:05:42 -0300

    Create nodes table and node schema.

---
+++ lib/sleeper_assignment/node.ex
@@ -0,0 +1,32 @@
+defmodule SleeperAssignment.Node do
+  @moduledoc """
+  Node model
+  """
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  alias SleeperAssignment.Cluster
+
+  @node_types ["duck", "goose"]
+  @node_status ["up", "down"]
+
+  schema "nodes" do
+    field :type, Ecto.Enum, values: @node_types, default: "duck"
+    field :status, Ecto.Enum, values: @node_status, default: "up"
+
+    belongs_to :cluster, Cluster
+    belongs_to :node, Node
+    belongs_to :network_partition, NetworkPartition
+
+    has_one :node, Node
+
+    timestamps()
+  end
+
+  def changeset(node, attrs) do
+    node
+    |> cast(attrs, [:type, :status, :cluster_id, :node_id, :network_partition_id])
+    |> validate_required([:type, :status, :cluster_id, :node_id, :network_partition_id])
+  end
+end
