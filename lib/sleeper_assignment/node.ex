ca3d91e8f53c267e54f96fc3af6136a0d7299795
Charles Furtado <charlees1509@hotmail.com>
2024-04-06 18:44:04 -0300

    Improve node schema.

--- lib/sleeper_assignment/node.ex
+++ lib/sleeper_assignment/node.ex
@@ -6,17 +6,17 @@ defmodule SleeperAssignment.Node do
 
   import Ecto.Changeset
 
-  alias SleeperAssignment.Cluster
+  alias SleeperAssignment.{Cluster, NetworkPartition, Node}
 
-  @node_types ["duck", "goose"]
-  @node_status ["up", "down"]
+  @node_types [:duck, :goose]
+  @node_status [:up, :down]
 
   schema "nodes" do
-    field :type, Ecto.Enum, values: @node_types, default: "duck"
-    field :status, Ecto.Enum, values: @node_status, default: "up"
+    field :type, Ecto.Enum, values: @node_types, default: :duck
+    field :status, Ecto.Enum, values: @node_status, default: :up
 
     belongs_to :cluster, Cluster
-    belongs_to :node, Node
+    belongs_to :child_node, Node
     belongs_to :network_partition, NetworkPartition
 
     has_one :node, Node
