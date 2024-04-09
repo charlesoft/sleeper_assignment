a9dc02f8f46cb4944a04077c6fb1eec0f6af78b6
Charles Furtado <charlees1509@hotmail.com>
2024-04-09 20:46:01 -0300

    Improve seeds data.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -7,16 +7,16 @@ cluster_two = Repo.insert!(%Cluster{name: "Cluster 2"})
 
 # Create network partitions for cluster one
 network_partition_one = Repo.insert!(%NetworkPartition{cluster_id: cluster_one.id})
+network_partition_two = Repo.insert!(%NetworkPartition{cluster_id: cluster_one.id})
 
 # Create network partitions for cluster two
 network_partition_three = Repo.insert!(%NetworkPartition{cluster_id: cluster_two.id})
-network_partition_four = Repo.insert!(%NetworkPartition{cluster_id: cluster_two.id})
 
 # Create nodes for cluster one
 node_one =
   Repo.insert!(%Node{
     type: :duck,
-    status: :up,
+    status: :down,
     cluster_id: cluster_one.id,
     network_partition_id: network_partition_one.id
   })
@@ -28,8 +28,7 @@ node_two =
     type: :duck,
     status: :down,
     cluster_id: cluster_one.id,
-    network_partition_id: network_partition_one.id,
-    parent_node_id: node_one.id
+    network_partition_id: network_partition_two.id
   })
 
 _server_two = Repo.insert!(%Server{port: Enum.random(4_000..10_000), node_id: node_two.id})
@@ -37,7 +36,7 @@ _server_two = Repo.insert!(%Server{port: Enum.random(4_000..10_000), node_id: no
 node_three =
   Repo.insert!(%Node{
     type: :duck,
-    status: :up,
+    status: :down,
     cluster_id: cluster_one.id
   })
 
@@ -46,7 +45,7 @@ _server_three = Repo.insert!(%Server{port: Enum.random(4_000..10_000), node_id: 
 node_four =
   Repo.insert!(%Node{
     type: :duck,
-    status: :up,
+    status: :down,
     cluster_id: cluster_one.id,
     parent_node_id: node_three.id
   })
@@ -56,7 +55,7 @@ _server_four = Repo.insert!(%Server{port: Enum.random(4_000..10_000), node_id: n
 node_five =
   Repo.insert!(%Node{
     type: :duck,
-    status: :up,
+    status: :down,
     cluster_id: cluster_one.id,
     parent_node_id: node_four.id
   })
@@ -66,7 +65,7 @@ _server_five = Repo.insert!(%Server{port: Enum.random(4_000..10_000), node_id: n
 node_six =
   Repo.insert!(%Node{
     type: :duck,
-    status: :up,
+    status: :down,
     cluster_id: cluster_one.id,
     parent_node_id: node_five.id
   })
@@ -76,7 +75,7 @@ _server_six = Repo.insert!(%Server{port: Enum.random(4_000..10_000), node_id: no
 node_seven =
   Repo.insert!(%Node{
     type: :duck,
-    status: :up,
+    status: :down,
     cluster_id: cluster_one.id,
     parent_node_id: node_six.id
   })
@@ -87,7 +86,7 @@ _server_seven = Repo.insert!(%Server{port: Enum.random(4_000..10_000), node_id: 
 node_eight =
   Repo.insert!(%Node{
     type: :duck,
-    status: :up,
+    status: :down,
     cluster_id: cluster_two.id,
     network_partition_id: network_partition_three.id
   })
@@ -97,10 +96,8 @@ _server_eight = Repo.insert!(%Server{port: Enum.random(4_000..10_000), node_id: 
 node_nine =
   Repo.insert!(%Node{
     type: :duck,
-    status: :up,
-    cluster_id: cluster_two.id,
-    network_partition_id: network_partition_three.id,
-    parent_node_id: node_eight.id
+    status: :down,
+    cluster_id: cluster_two.id
   })
 
 _server_nine = Repo.insert!(%Server{port: Enum.random(4_000..10_000), node_id: node_nine.id})
@@ -108,9 +105,8 @@ _server_nine = Repo.insert!(%Server{port: Enum.random(4_000..10_000), node_id: n
 node_ten =
   Repo.insert!(%Node{
     type: :duck,
-    status: :up,
+    status: :down,
     cluster_id: cluster_two.id,
-    network_partition_id: network_partition_three.id,
     parent_node_id: node_nine.id
   })
 
@@ -119,9 +115,8 @@ _server_ten = Repo.insert!(%Server{port: Enum.random(4_000..10_000), node_id: no
 node_eleven =
   Repo.insert!(%Node{
     type: :duck,
-    status: :up,
+    status: :down,
     cluster_id: cluster_two.id,
-    network_partition_id: network_partition_three.id,
     parent_node_id: node_ten.id
   })
 
@@ -130,9 +125,9 @@ _server_eleven = Repo.insert!(%Server{port: Enum.random(4_000..10_000), node_id:
 node_twelve =
   Repo.insert!(%Node{
     type: :duck,
-    status: :up,
+    status: :down,
     cluster_id: cluster_two.id,
-    network_partition_id: network_partition_four.id
+    network_partition_id: network_partition_three.id
   })
 
 _server_twelve = Repo.insert!(%Server{port: Enum.random(4_000..10_000), node_id: node_twelve.id})
